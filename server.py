d80695d13f394620c766cc1a27c3f8357d9b455f
Ismael Faro <Ismael.Faro1@ibm.com>
2021-10-13 02:55:18 -0400

    update server

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 import logging
 from websocket_server import WebsocketServer
 
+def message_received(client, server, message):
+	server.send_message_to_all(message)
+
 server = WebsocketServer(8080, host='127.0.0.1', loglevel=logging.INFO)
+server.set_fn_message_received(message_received)
 server.run_forever()
