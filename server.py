6fcbb74c9146e120d5262002fb9357ec547c55bf
Ismael Faro <Ismael.Faro1@ibm.com>
2021-10-14 11:16:06 -0400

    update service

--- server.py
+++ server.py
@@ -15,9 +15,10 @@ def message_received(client, server, message):
 	print(message)
 	server.send_message_to_all(message)
 
-server = WebsocketServer(8080, host='localhost', loglevel=logging.INFO)
+server = WebsocketServer(8080, host='0.0.0.0', loglevel=logging.INFO)
 server.set_fn_message_received(message_received)
 server.set_fn_new_client(connected)
 server.set_fn_client_left(handle_close)
 
 server.run_forever()
+
